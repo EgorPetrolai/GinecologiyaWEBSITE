f084e6bebc5589718fbc15808b11c50bab44e08e
Egor <lindos228@gmail.com>
2023-09-20 16:19:25 +0300

    first

--- main.py
+++ main.py
@@ -1,16 +1,51 @@
-# This is a sample Python script.
+# Открываем файл для чтения
+with open('data.txt', 'r', encoding='utf-8') as file:
+    # Читаем содержимое файла и разделяем его по запятым
+    data = file.read().split(',')
+print(data)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+<table>
+    <tbody>
+        <tr>
+            <td colspan="2" width="623">
+                <h5 style="text-align: center;"><a href="{0}"></a></h5>
+                <h5 style="text-align: center;"><a href="{1}"></a></h5>
+                <h5 style="text-align: center;"><a href="{2}"></a></h5>
+                <h5 style="text-align: center;"><a href="{3}"></a></h5>
+                <h5 style="text-align: center;"><a href="{4}">0</a></h5>
+            </td>
+        </tr>
+        <tr>
+            <td width="311">
+                <h5 style="text-align: center;"><a href="{5}"></a></h5>
+                <h5 style="text-align: center;"><a href="{6}">02.10.2023</a></h5>
+                <h5 style="text-align: center;"><a href="{7}">02.10.2023</a></h5>
+                <h5 style="text-align: center;"><a href="{8}">02.10.2023</a></h5>
+                <h5 style="text-align: center;"><a href="{9}">02.10.2023</a></h5>
+            </td>
+            <td width="312">
+                <h5 style="text-align: center;"><a href="{10}">02.10.2023</a></h5>
+                <h5 style="text-align: center;"><a href="{12}">02.10.2023</a></h5>
+                <h5 style="text-align: center;"><a href="{13}">02.10.2023</a></h5>
+                <h5 style="text-align: center;"><a href="{14}">02.10.2023</a></h5>
+                <h5 style="text-align: center;"><a href="{15}">02.10.2023</a></h5>
+            </td>
+        </tr>
+        <tr>
+            <td style="text-align: center;" width="311">
+                <h5 style="text-align: center;"><a href="{16}">02.10.2023</a></h5>
+                <h5 style="text-align: center;"><a href="{17}">02.10.2023</a></h5>
+                <h5 style="text-align: center;"><a href="{18}">02.10.2023</a></h5>
+                <h5 style="text-align: center;"><a href="{19}">02.10.2023</a></h5>
+                <h5 style="text-align: center;"><a href="{20}">02.10.2023</a></h5>
+            </td>
+            <td width="312">
+                <h5 style="text-align: center;"><a href="{21}">02.10.2023</a></h5>
+                <h5 style="text-align: center;"><a href="{22}">02.10.2023</a></h5>
+                <h5 style="text-align: center;"><a href="{23}">02.10.2023</a></h5>
+                <h5 style="text-align: center;"><a href="{24}">02.10.2023</a></h5>
+                <h5 style="text-align: center;"><a href="{25}">02.10.2023</a></h5>
+            </td>
+        </tr>
+    </tbody>
+</table>
